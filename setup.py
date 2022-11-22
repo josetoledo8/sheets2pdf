59b9b9e5beee3dd3ff5460e86db7137df83e2bba
josetoledo8 <jose.toledo.bc@gmail.com>
2022-11-22 12:05:37 -0300

    .

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='sheets2pdf',
     version='0.0.1',
     author='José Roberto de Toledo',
     author_email='jose.toledo@institutosingular.org',
-    url='https://github.com/josetoledo8/sheets2pdf',
-    packages=['sheets2pdf'],
     description='Testing installation of Package',
-    long_description='Long description',
+    long_description=long_description,
     long_description_content_type="text/markdown",
+    url='https://github.com/josetoledo8/sheets2pdf',
+    project_urls = {
+        "Repo link": "https://github.com/josetoledo8/sheets2pdf"
+    },
+    license='MIT',
+    packages=['sheets2pdf'],
     install_requires=[ 
-        'pandas', 
-        'google-api-core', 
-        'google-api-python-client',
-        'google-auth',
-        'google-auth-oauthlib',
-        'fpdf'
-        ],
+    'pandas', 
+    'google-api-core', 
+    'google-api-python-client',
+    'google-auth',
+    'google-auth-oauthlib',
+    'fpdf'
+    ],
 )
