2b7708de8bd23ec1d3ba83f82ce3672977e6e00a
josetoledo8 <jose.toledo.bc@gmail.com>
2022-11-22 10:15:41 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setuptools.setup(
     author_email='jose.toledo@institutosingular.org',
     url='https://github.com/josetoledo8/sheets2pdf',
     packages=['sheets2pdf'],
+    description='Testing installation of Package',
+    long_description='Long description',
+    long_description_content_type="text/markdown",
     install_requires=[ 
         'pandas', 
         'google-api-core', 
